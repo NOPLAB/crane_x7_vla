e2788317a36f55edc52e36a85e0917bde4f47751
nop <noplab90@gmail.com>
2025-09-24 15:45:11 +0900

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,23 @@
 
 DOCKER_IMAGE_NAME=ros-dev
 DOCKER_CONTAINER_NAME=ros-dev
-DISPLAY=host.docker.internal:0.0
+
+DOCKER_OPTION=
+DOCKER_WSL_OPTION="-v /tmp/.X11-unix:/tmp/.X11-unix -v /mnt/wslg:/mnt/wslg"
+
+if [[ "$(uname -r)" == *-microsoft-standard-WSL2 ]]; then
+    # WSL
+    DOCKER_OPTION=$DOCKER_WSL_OPTION
+else
+    # Other
+    DISPLAY=host.docker.internal:0.0
+fi
 
 docker run \
     -e DISPLAY=$DISPLAY	\
     --name=$DOCKER_CONTAINER_NAME \
     --rm \
     -it \
+    $DOCKER_OPTION \
     $DOCKER_IMAGE_NAME
 
