283988f540a9ee77ff282c8132004346de0c59bf
nop <noplab90@gmail.com>
2025-09-24 15:38:15 +0900

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-DOCKER_IMAGE_NAME=ros
+DOCKER_IMAGE_NAME=ros-dev
 DOCKER_CONTAINER_NAME=ros-dev
+DISPLAY=host.docker.internal:0.0
 
-docker run --rm -e DISPLAY=$DISPLAY --name $DOCKER_CONTAINER_NAME -it $DOCKER_IMAGE_NAME
+docker run \
+    -e DISPLAY=$DISPLAY	\
+    --name=$DOCKER_CONTAINER_NAME \
+    --rm \
+    -it \
+    $DOCKER_IMAGE_NAME
 
